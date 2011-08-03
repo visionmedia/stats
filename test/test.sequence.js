079fe683b8abee60e87a1550c5a5566e034134c0
Tj Holowaychuk <tj@vision-media.ca>
2011-08-03 05:45:38 -0700

    visit seq

---
+++ test/test.sequence.js
@@ -0,0 +1,11 @@
+
+/**
+ * Module dependencies.
+ */
+
+var jss = require('../')
+  , common = require('./common');
+
+var stats = jss.stats('("foo", "bar", "baz")');
+stats.should.have.property('statements', 1);
+stats.should.have.property('strings', 3);
