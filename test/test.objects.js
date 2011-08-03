96a696860a7ff6344afc0395d97a6d16ca3257aa
Tj Holowaychuk <tj@vision-media.ca>
2011-08-03 04:54:16 -0700

    fixed object test

--- test/test.objects.js
+++ test/test.objects.js
@@ -6,6 +6,6 @@
 var jss = require('../')
   , common = require('./common');
 
-var stats = jss.stats(fixture('large.js'));
+var stats = jss.stats(fixture('objects.js'));
 stats.should.have.property('objectLiterals', 2);
 stats.should.have.property('objectsCreated', 0);
