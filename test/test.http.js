7525c695c688e6cdf26e13fdcd8d6c044bff32d0
Tj Holowaychuk <tj@vision-media.ca>
2011-08-03 04:38:05 -0700

    Added "numbers" and "strings"

--- test/test.http.js
+++ test/test.http.js
@@ -9,6 +9,8 @@ var jss = require('../')
 jss.should.have.property('version');
 
 var stats = jss.stats(fixture('http.js'));
+stats.should.have.property('strings', 4);
+stats.should.have.property('numbers', 1);
 stats.should.have.property('statements', 4);
 stats.should.have.property('assignments', 1);
 stats.should.have.property('loc', 11);
